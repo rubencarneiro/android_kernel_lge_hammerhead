02264ee658fb3f71a40f9650c55522f95125b8dc
rubencarneiro <rubencarneiro01@gmail.com>
2020-10-22 11:27:39 +0100

    Wlan drivers as module

--- arch/arm/mach-msm/lge/devices_lge.c
+++ arch/arm/mach-msm/lge/devices_lge.c
@@ -19,6 +19,7 @@
 #include <linux/persistent_ram.h>
 #include <linux/io.h>
 #include <linux/slab.h>
+#include <linux/module.h> 
 #include <asm/setup.h>
 #include <asm/system_info.h>
 #include <mach/board_lge.h>
@@ -187,6 +188,8 @@ enum lge_boot_mode_type lge_get_boot_mode(void)
 {
 	return lge_boot_mode;
 }
+
+EXPORT_SYMBOL(lge_get_boot_mode);
 
 /* for board revision */
 static hw_rev_type lge_bd_rev = HW_REV_B;
